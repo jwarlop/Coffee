07eff0985c8ff80be368329fc8045ad7cd7ea74b
jwarlop <jwarlop@gmail.com>
2018-06-23 18:57:37 -0700

    added h1>

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def coffee():
-  return 'Hello coffee team members: Kevin, Gary, Dutch and John'
+  return '<h1>Hello coffee team members: Kevin, Gary, Dutch and John</h1>'
 
